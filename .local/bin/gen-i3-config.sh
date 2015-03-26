21a326f764da2c8add98bc0cac834e127c9a9387
Lefteris Karapetsas <lefteris@refu.co>
2015-03-26 10:03:58 +0100

    Adding the i3 blur lock script

--- .local/bin/gen-i3-config.sh
+++ .local/bin/gen-i3-config.sh
@@ -2,10 +2,11 @@
 
 # generate machine specific i3 configuration file
 cat ~/.i3/i3config.template > ~/.i3/config
-# for the laptop at work, setup the dual monitor setup
+# append specific work laptop stuff
 if [ $HOSTNAME == "archlenovo" ]; then
    cat ~/.i3/i3workconfig.template >> ~/.i3/config
 fi
+# append specific home computer stuff
 if [ $HOSTNAME == "archdesktop" ]; then
    cat ~/.i3/i3homeconfig.template >> ~/.i3/config
 fi
